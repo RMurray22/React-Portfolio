3ec2a8c82a4c7f1853c995bd66a4a3b43b3b7dc8
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-06-10 20:48:08 -0700

    changed image of project 1 to jpeg

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "The Magic Eat Ball",
     subtitle: "MERN Stack",
     description:"A full stack web application that will randomly generate a restaurant based on the user's location. The user can also save their favorite restaurants to their profile.(Future development).",
-    image: "./Screenshot 2023-06-10 at 10.56.59 AM.png",
+    image: "./Screenshot 2023-06-10 at 5.11.26.PM.jpg",
     link: "https://lit-brushlands-86158.herokuapp.com/",
   }];
   
