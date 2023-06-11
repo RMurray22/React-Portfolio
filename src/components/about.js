4808692ba4e704a7f85e8d6130627a33baa102a1
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-06-11 00:45:08 -0700

    adjusted header and hire mebutton color

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ export default function About() {
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Rich.
             <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            apps and websites.
           </h1>
           <p className="mb-8 leading-relaxed">
             
