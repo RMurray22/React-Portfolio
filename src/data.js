1b263c137dac33e3dace4a3741081548fb2368e5
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-06-10 21:00:59 -0700

    renamed image

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "The Magic Eat Ball",
     subtitle: "MERN Stack",
     description:"A full stack web application that will randomly generate a restaurant based on the user's location. The user can also save their favorite restaurants to their profile.(Future development).",
-    image: "./Screenshot 2023-06-10 at 5.11.26.PM.jpg",
+    image: "./public/project-1.jpg",
     link: "https://lit-brushlands-86158.herokuapp.com/",
   }];
   
