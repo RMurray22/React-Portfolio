5f63466a53d612ddf62281dffc52ed5b71c2f022
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-06-12 17:13:59 -0700

    added images folder and updated code in data

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "The Magic Eat Ball",
     subtitle: "MERN Stack",
     description:"A full stack web application that will randomly generate a restaurant based on the user's location. The user can also save their favorite restaurants to their profile.(Future development).",
-    image: "./public/project-1.jpg",
+    image: <img src={require('/images/project-1.jpg')} /> ,  
     link: "https://lit-brushlands-86158.herokuapp.com/",
   }];
   
