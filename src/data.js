97526de591565a5bf0605658c56dfa7eb10e44fe
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-07-15 23:46:28 -0700

    Updated data code an added screenshot image

--- src/data.js
+++ src/data.js
@@ -5,6 +5,13 @@ export const projects = [
     description:"A full stack web application that will randomly generate a restaurant based on the user's location. The user can also save their favorite restaurants to their profile.(Future development).",
     image: "./project-1.jpg",
     link: "https://lit-brushlands-86158.herokuapp.com/",
+  },
+  {
+    title: "Team Tracker",
+    subtitle: "Node.js",
+    description:"A command line application that allows the user to create a team profile. The user can add a manager, engineer, and intern to the team. The user can also add an email and GitHub username to the engineer and intern.",
+    image: "./Screenshot 2023-05-31 10.56.45.png",
+    link: "https://drive.google.com/file/d/1Lta29NUoKEafvP-tWkX-iRzndqR3BRcG/view?usp=drive_link"
   }];
   
 export const skills = [
