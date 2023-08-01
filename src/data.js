5f562009b6a0bfe5a474bf9fa7935ad8239474af
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-08-01 14:39:46 -0700

    added password generator to data.js and added photo

--- src/data.js
+++ src/data.js
@@ -12,7 +12,14 @@ export const projects = [
     description:"A command line application that allows the user to create a team profile. The user can add a manager, engineer, and intern to the team. The user can also add an email and GitHub username to the engineer and intern.",
     image: "./Team Builder.png",
     link: "https://drive.google.com/file/d/1Lta29NUoKEafvP-tWkX-iRzndqR3BRcG/view?usp=drive_link"
-  }];
+  },
+  {
+    title: "Password Generator",
+    subtitle: "Html, CSS, JavaScript",
+    description:"A password generator that will generate a random password based on the user's criteria. The user can choose the length of the password and if they want to include uppercase, lowercase, numbers, and/or special characters.",
+    image: "./Password Generator.png",
+    link: "https://rmurray22.github.io/Generator-2.0/",
+}];
   
 export const skills = [
   "HTML",
